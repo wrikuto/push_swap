e04c0ea4a05e1d3c4e82956f7e896f26196148f3
Wakita Rikuto <wrikuto@c1r26s10.42tokyo.jp>
2023-08-01 22:21:38 +0900

    auto push

--- split_new_split.c
+++ split_new_split.c
@@ -6,7 +6,7 @@
 /*   By: wrikuto <wrikuto@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 15:46:29 by wrikuto           #+#    #+#             */
-/*   Updated: 2023/08/01 20:06:14 by wrikuto          ###   ########.fr       */
+/*   Updated: 2023/08/01 22:21:06 by wrikuto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 #include <stdio.h>
 
-static void	get_centers_num(t_node *stack_a, int *sp_num)
+static void	get_cep_num(t_node *stack_a, int *sn, size_t n)
 {
-	int		*num;
-	size_t	len;
-	size_t	i;
+	int			*num;
+	size_t		len;
+	size_t		i;
 
 	i = 1;
 	len = stack_len(stack_a);
@@ -31,50 +31,61 @@ static void	get_centers_num(t_node *stack_a, int *sp_num)
 		num[i] = get_min_more(stack_a, num[i - 1]);
 		i++;
 	}
-	i = 0
-	while (i < n)
-	{
-		sp_num[i] = num[len / (n * (i + 1)];
-		j++;
-	}
+	i = -1;
+	while (i++ < n - 1)
+		sn[i] = num[len / (n * (i + 1))];
+	sn[n - 1] = INT_MAX;
+	for (i = 0; i < len; i++)
+		printf("%d\n", num[i]);
 	free(num);
 }
 
-// static void	groping_last(t_node **stack_a, t_node **stack_b, int mid)
-// {
-// 	while (*stack_a != NULL)
-// 	{
-// 		if ((*stack_a)->num < mid)
-// 			pb(stack_a, stack_b);
-// 		else
-// 			pb_rb(stack_a, stack_b);
-// 	}
-// }
+void	push_to_b(t_node **stack_a, t_node **stack_b, size_t n, int *sn)
+{
+	int		j;
+	size_t	len;
+	size_t	count;
+	size_t	push_count;
+
+	j = 0;
+	count = 0;
+	len = stack_len(*stack_a);
+	while (*stack_a)
+	{
+		while (count < len)
+		{
+			if ((*stack_a)->num <= sn[j * 2])
+				pb(stack_a, stack_b);
+			else if ((*stack_a)->num > sn[j * 2] && (*stack_a)->num <= sn[j * 2 + 1])
+				pb_rb(stack_a, stack_b);
+			else
+				ra(stack_a);
+			count++;
+		}
+		j++;
+		count = 0;
+		len = stack_len(*stack_a);
+	}
+	grope_last(stack_a, stack_b, sn[n - 2])
+}
 
 static void	groping(t_node **stack_a, t_node **stack_b)
 {
-	size_t	count;
-	int		*sp_num;
+	size_t	n;
+	int		*sn;
 	size_t	len;
 
-	sp_num = malloc(sizeof(int) * 3);
-	if (sp_num == NULL)
-		exit(1);
-	get_centers_num(*stack_a, sp_num);
 	len = stack_len(*stack_a);
-	count = 0;
-	while (count <= len)
-	{
-		if ((*stack_a)->num < sp_num[0])
-			pb(stack_a, stack_b);
-		else if ((*stack_a)->num >= sp_num[0] && (*stack_a)->num < sp_num[1])
-			pb_rb(stack_a, stack_b);
-		else
-			ra(stack_a);
-		count++;
-	}
-	groping_last(stack_a, stack_b, sp_num[2]);
-	free(sp_num);
+	if (len < 100)
+		n = 4;
+	else
+		n = 4 * (len / 100);
+	sn = malloc(sizeof(int) * n);
+	if (sn == NULL)
+		exit(1);
+	get_cep_num(*stack_a, sn, n);
+	push_to_b(stack_a, stack_b, n, sn);
+	free(sn);
 }
 
 static int	len_to_max(t_node *stack, int max)
