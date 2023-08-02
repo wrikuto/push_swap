245405fcb7486c3b12a3b0fbd3c494ba73c146d9
Wakita Rikuto <wrikuto@c1r15s7.42tokyo.jp>
2023-08-02 15:57:08 +0900

    auto push

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: wrikuto <wrikuto@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 17:16:38 by wrikuto           #+#    #+#             */
-/*   Updated: 2023/08/02 01:21:23 by wrikuto          ###   ########.fr       */
+/*   Updated: 2023/08/02 15:57:03 by wrikuto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,45 +27,30 @@ typedef struct s_node
 	struct s_node	*prev;
 }	t_node;
 
-// chk
 int			chk_args(char **argv);
-// chk_utils
 int			chk_num_string(char *str);
 long		string_atol(char **str);
 long		ps_atol(const char *str);
 int			ft_isdigit(int c);
 long		ps_atol(const char *str);
 int			is_num(char *str);
-
-// list_utils
 t_node		*free_list(t_node **head);
 void		create_list(char **argv, t_node **head);
-
-// swap
 void		sa(t_node **head);
 void		sb(t_node **head);
 void		ss(t_node **list_a, t_node **list_b);
-
-// rotate
 void		ra(t_node **head);
 void		rb(t_node **head);
 void		rr(t_node **stack_a, t_node **stack_b);
-
-// reverse
 void		rra(t_node **head);
 void		rrb(t_node **head);
 void		rrr(t_node **stack_a, t_node **stack_b);
-
-// push
 void		push(t_node **src, t_node **dest);
 void		pa(t_node **stack_a, t_node **stack_b);
 void		pb(t_node **stack_a, t_node **stack_b);
-
-// sort
 void		sort_three_elements(t_node **stack_a);
 void		sort_five_or_four(t_node **stack_a, t_node **stack_b, int count);
 void		quick_insert(t_node **stack_a, t_node **stack_b);
-// sort utils
 int			get_max(t_node *stack);
 int			get_min(t_node *stack);
 long int	ft_abs(long int n);
